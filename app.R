2a2111a81e7019bb49e82219c682738e68ae83ba
Will Moir <wgmoir@gmail.com>
2017-08-04 20:11:45 -0400

    updated reactive - still need to fix y-axis updating

--- app.R
+++ app.R
@@ -1,7 +1,7 @@
-
 library(shiny)
 
-ui <- fluidPage(
+ui <- shinyUI(
+  fluidPage(
   titlePanel("Which P-Values Can You Expect?"),
   sidebarLayout(
     sidebarPanel(
@@ -13,8 +13,7 @@ ui <- fluidPage(
       numericInput("bars", "Number of bars in histogram: ", 20),
       sliderInput("xlim", "Range of x-axis", min = 0, max = 1,
                   value = c(0, 1)),
-      sliderInput("ylim", "Range of y-axis", min = 0, max = 100000,
-                  value = c(0, 100000), sep=""),
+      uiOutput("ylim"),
       p("The idea and code for this simulation are by Daniel Lakens, and are part of his Coursera course",
         a("Improving your statistical inferences",
           href = "https://www.coursera.org/learn/statistical-inferences/",
@@ -23,15 +22,21 @@ ui <- fluidPage(
     ),
     mainPanel(plotOutput("pplot"))
   )
-)
+))
 
 
-server <- function(input, output) {
+server <- shinyServer(
+  function(input, output, session) {
   #Load pwr package to easily calculate the statistical power
   if(!require(pwr)){install.packages('pwr')}
   library(pwr)
   #Disable scientific notation (1.05e10)
   options(scipen=999)
+  
+  output$ylim <- renderUI({
+    sliderInput("ylim", "Range of y-axis", min = 0, max = input$nSims,
+                value = c(0, input$nSims), sep="")
+  })
   
   dataInput <- eventReactive(input$go, {
     nSims <-input$nSims #number of simulated experiments
@@ -43,22 +48,25 @@ server <- function(input, output) {
     #With a mean difference of 6, and SD of 15, and a sample size of 26, the test has 50% power)
     
     p <-numeric(nSims) #set up empty variable to store all simulated p-values
-
+    
     #Run simulation
     for(i in 1:nSims){ #for each simulated experiment
       x<-rnorm(n = n, mean = M, sd = SD) #Simulate data with specified mean, standard deviation, and sample size
       z<-t.test(x, mu=100) #perform the t-test against mu (set to value you want to test against)
       p[i]<-z$p.value #get the p-value and store it
     }
-    p
-  })
+    p 
+  }, ignoreNULL = FALSE )
   
-  
+ nSimsInput <- eventReactive(input$go, {
+    input$nSims #number of simulated experiments
+  }, ignoreNULL = FALSE )
+ 
   output$pplot <- renderPlot({
     
-
+    
     bars<-input$bars
-    nSims <-input$nSims #number of simulated experiments
+    nSims <-nSimsInput() #number of simulated experiments
     #Set number of simulations
     
     M<-input$M #Mean IQ score in the sample (will be compared with 100 in a one-sample t-test)
@@ -77,13 +85,12 @@ server <- function(input, output) {
          col="grey", xlim=c(input$xlim[1],input$xlim[2]),  ylim=c(input$ylim[1], input$ylim[2]))
     axis(side=1, at=seq(0,1, 0.1), labels=seq(0,1,0.1))
     axis(side=2, at=seq(0,nSims, nSims/4), labels=seq(0,nSims, nSims/4), las=2)
-    abline(h=10000/bars, col = "red", lty=3)
+    abline(h=nSims/bars, col = "red", lty=3)
     #dev.off()
     
     #© Daniel Lakens, 2016. 
     # This work is licensed under a Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. https://creativecommons.org/licenses/by-nc-sa/4.0/
   })
-}
-
+})
 
 shinyApp(ui = ui, server = server)
